48dd90a9f56776ae1fc2ff11574717a2f97d7114
Студент <student@BT.local>
2026-06-04 12:16:39 +0300

    Добавлен массив с синусом

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
- Начальный файл
+﻿ Начальный файл
+#include <iostream>
+#include <cmath>
+int main() {
+    double arr[10];
+    for (int i = 0; i < 10; i++) {
+        arr[i] = sin(i);
+    }
+    for (int i = 0; i < 10; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
